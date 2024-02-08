92cfa628d726de0ae0cc7ce82dc4d459c3e5d85c
Amit Vishwakarma <amit.excellence@gmail.com>
2024-02-08 11:34:36 +0530

    setup pg database

--- blog-api/index.ts
+++ blog-api/index.ts
@@ -1,8 +1,10 @@
 import "dotenv/config";
 import app from "./src/App";
+import { Database } from "./src/database/Database";
 
-const PORT = parseInt(process.env.PORT || "5000");
+const PORT = parseInt(process.env.PORT || "4000");
 
 app.listen(PORT, "0.0.0.0", async () => {
+  await Database.init();
   console.info(`Server is running on: ${PORT}`);
 });
---
+++ blog-api/src/database/Database.ts
@@ -0,0 +1,30 @@
+import { Sequelize } from "sequelize";
+
+export class Database {
+  public static sequelize: Sequelize = new Sequelize(
+    process.env.PG_DATABASE || "blog-db",
+    process.env.PG_USER || "pguser",
+    process.env.PG_PASSWORD || "pgpassword",
+    {
+      host: process.env.PG_HOST || "localhost",
+      dialect: "postgres",
+    },
+  );
+  public static async init(): Promise<boolean> {
+    try {
+      await Database.sequelize.authenticate();
+      await Database.sequelize.sync();
+      console.log("Connection has been established successfully.");
+      return true;
+    } catch (error) {
+      console.error("Unable to connect to the database:", error);
+      return false;
+    }
+  }
+
+  public static async disconnect(): Promise<void> {
+    await Database.sequelize?.close();
+  }
+}
+
+export default Database;
